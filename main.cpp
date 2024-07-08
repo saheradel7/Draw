6d3c2d145dc8d17fa550729974d4cc62a5f2f53b
Saher <saher.adel@insys-eg.com>
2024-07-08 15:11:28 +0300

    A letter

--- main.cpp
+++ main.cpp
@@ -1,13 +1,37 @@
 #include<iostream>
 using namespace std;
 
-
+void a(int w, int h);
 void s(int w, int h);
 
 int main()
 {
+    a(7,7);
 
+    cout<<endl<<endl<<endl;
 }
+void a(int w, int h)
+{
+    for(int i=0;i<h ; i++)
+    {
+        for(int j =0 ; j< w ; j++)
+        {
+            if(
+                (i==0)||
+                (i==(h/2))||
+                (j==0)||
+                (j==w-1)
+            ){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
+
 
 void s(int w , int h)
 {
