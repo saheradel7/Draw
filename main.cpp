aa2bfa4e1df51697683226e79704fb0618786983
Saher <saher.adel@insys-eg.com>
2024-07-10 11:29:43 +0300

    letter E

--- main.cpp
+++ main.cpp
@@ -6,13 +6,37 @@ void s(int w, int h);
 void b(int w , int h);
 void c(int h, int w);
 void d(int h , int w);
+void e(int w, int h);
 
 int main()
 {
-    d(18,17);
+    e(10,10);
 
     cout<<endl<<endl<<endl;
 }
+void e(int w, int h)
+{
+    for(int i =0 ; i<h ;i++)
+    {
+        for(int j=0;j<w;j++)
+        {
+            if(
+                (i==0)
+                ||(i==h/2)
+                ||(i==h-1)
+                ||(j==0)
+            )
+            {
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
+
 
 void d(int h , int w)
 {
